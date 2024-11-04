cc00e2618961d7fcabe8171ae92800968a36b845
Tomiwa Aderibigbe <tomiwa95@gmail.com>
2024-11-04 08:36:46 +0100

    Finighed with testimonial section on mobile view, and modified container

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -44,27 +44,38 @@ const testimonials = [
 export const TestimonialsSection = () => {
 	return (
 		<div className='py-16'>
-			<div className='comtainer'>
+			<div className='container'>
 				<SectionHeader
 					eyebrow='Happy Clients'
 					title='What Clients Say About Me'
 					description="Don't just take my word for it. See what my clients have to say about my work."
 				/>
-				<div>
-					{testimonials.map((testimonial) => (
-						<Card
-							key={testimonial.name}
-							className='bg-gray-800 rounded-3xl p-6 relative overflow-hidden z-0'
-						>
-							<Image
-								src={testimonial.avatar}
-								alt={testimonial.name}
-							/>
-							<div>{testimonial.name}</div>
-							<div>{testimonial.position}</div>
-							<p>{testimonial.text}</p>
-						</Card>
-					))}
+				<div className='mt-16 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]'>
+					<div className='flex gap-8 flex-none'>
+						{testimonials.map((testimonial) => (
+							<Card
+								key={testimonial.name}
+								className='max-w-xs'
+							>
+								<div className='flex gap-4 items-center'>
+									<div className='size-14 bg-gray-700 inline-flex items-center justify-center rounded-full flex-shrink-0'>
+										<Image
+											src={testimonial.avatar}
+											alt={testimonial.name}
+											className='max-h-full'
+										/>
+									</div>
+									<div>
+										<div className='font-semibold'>{testimonial.name}</div>
+										<div className='text-sm text-white/40'>
+											{testimonial.position}
+										</div>
+									</div>
+								</div>
+								<p className='mt-4 text-sm'>{testimonial.text}</p>
+							</Card>
+						))}
+					</div>
 				</div>
 			</div>
 		</div>
