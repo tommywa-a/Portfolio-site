c82054dbe06cbf4f28b34052a17d53c7dfb438eb
Tomiwa Aderibigbe <tomiwa95@gmail.com>
2024-11-08 15:52:38 +0100

    Finshed with static Map card in About section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -46,44 +46,44 @@ const hobbies = [
 	{
 		title: 'Painting',
 		emoji: '🎨',
-    left: '5%',
-    top: '5%',
+		left: '5%',
+		top: '5%',
 	},
 	{
 		title: 'Photography',
 		emoji: '📸',
-    left: '50%',
-    top: '5%',
+		left: '50%',
+		top: '5%',
 	},
 	{
-    title: 'Hiking',
+		title: 'Hiking',
 		emoji: '🥾',
-    left: '35%',
-    top: '40%',
+		left: '35%',
+		top: '40%',
 	},
-  {
-    title: 'Gaming',
-    emoji: '🎮',
-    left: '10%',
-    top: '35%',
-  },
+	{
+		title: 'Gaming',
+		emoji: '🎮',
+		left: '10%',
+		top: '35%',
+	},
 	{
 		title: 'Music',
 		emoji: '🎵',
-    left: '70%',
-    top: '45%',
+		left: '70%',
+		top: '45%',
 	},
 	{
 		title: 'Fitness',
 		emoji: '🏋️‍♂️',
-    left: '5%',
-    top: '65%',
+		left: '5%',
+		top: '65%',
 	},
 	{
 		title: 'Reading',
 		emoji: '📖',
-    left: '45%',
-    top: '70%',
+		left: '45%',
+		top: '70%',
 	},
 ]
 
@@ -98,7 +98,10 @@ export const AboutSection = () => {
 				/>
 				<div className='mt-20 flex flex-col gap-8'>
 					<Card className='h-[320px]'>
-						<CardHeader title='My Reads' description='Explore the books shaping my perspectives.' />
+						<CardHeader
+							title='My Reads'
+							description='Explore the books shaping my perspectives.'
+						/>
 						<div className='w-40 mx-auto mt-8'>
 							<Image
 								src={bookImage}
@@ -107,31 +110,56 @@ export const AboutSection = () => {
 						</div>
 					</Card>
 					<Card className='h-[320px] p-0'>
-          <CardHeader title='My Toolbox' description='Explore the technologies and tools I use to craft digital
-								experiences.' className='px-6 pt-6'/>
-						<ToolboxItems items={toolboxItems} className='mt-6' />
-						<ToolboxItems items={toolboxItems} className='mt-6' itemWrapperClassName='-translate-x-1/2'/>
+						<CardHeader
+							title='My Toolbox'
+							description='Explore the technologies and tools I use to craft digital
+								experiences.'
+							className='px-6 pt-6'
+						/>
+						<ToolboxItems
+							items={toolboxItems}
+							className='mt-6'
+						/>
+						<ToolboxItems
+							items={toolboxItems}
+							className='mt-6'
+							itemWrapperClassName='-translate-x-1/2'
+						/>
 					</Card>
 					<Card className='h-[320px] p-0 flex flex-col'>
-          <CardHeader title='Beyond the Code' description='Explore my interests and hobbies beyond the digital realm.'  className='px-6 py-6'/>
+						<CardHeader
+							title='Beyond the Code'
+							description='Explore my interests and hobbies beyond the digital realm.'
+							className='px-6 py-6'
+						/>
 						<div className='relative flex-1'>
 							{hobbies.map((hobby) => (
-								<div key={hobby.title} className='inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute' style={{left: hobby.left, top: hobby.top}}>
-									<span className='font-medium text-gray-950'>{hobby.title}</span>
+								<div
+									key={hobby.title}
+									className='inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute'
+									style={{ left: hobby.left, top: hobby.top }}
+								>
+									<span className='font-medium text-gray-950'>
+										{hobby.title}
+									</span>
 									<span>{hobby.emoji}</span>
 								</div>
 							))}
 						</div>
 					</Card>
-					<Card>
+					<Card className='h-[320px] p-0 relative'>
 						<Image
 							src={mapImage}
 							alt='Map'
+							className='h-full w-full object-cover object-left-top'
 						/>
-						<Image
-							src={smileMemoji}
-							alt='Smiling Memoji'
-						/>
+						<div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[""] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30'>
+							<Image
+								src={smileMemoji}
+								alt='Smiling Memoji'
+                className='size-20'
+							/>
+						</div>
 					</Card>
 				</div>
 			</div>
