c9791202bbc53f4ab1e9fbf5fdb19292d7284e47
Tomiwa Aderibigbe <tomiwa95@gmail.com>
2024-11-19 12:45:08 +0100

    Added move-left animation for tape section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,6 +27,7 @@ const config: Config = {
 			},
 			animation: {
 				'ping-large': 'ping-large 1s ease-in-out infinite',
+				'move-left': 'move-left 1s linear infinite',
 			},
 			keyframes: {
 				'ping-large': {
@@ -35,6 +36,14 @@ const config: Config = {
 						opacity: '0',
 					},
 				},
+				'move-left': {
+					'0%': {
+						transform: 'translateX(0%)',
+					},
+					'100%': {
+						transform: 'translateX(-50%)',
+					}
+				}
 			},
 		},
 	},
